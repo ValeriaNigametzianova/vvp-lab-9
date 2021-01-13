2473c0f06cb6bc28d12eeac2c7cd8b3021066408
ValeriaNigametzianova <71436617+ValeriaNigametzianova@users.noreply.github.com>
2021-01-13 10:58:20 +0300

    Add files via upload

---
+++ №1.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int second;
+	printf("How many seconds have passed?\n");
+	scanf_s("%d", &second);
+
+	int secondInLastMinute;
+	secondInLastMinute = second % 60;
+	printf("%d second have passed of last minute", secondInLastMinute);
+	return 0;
+}
