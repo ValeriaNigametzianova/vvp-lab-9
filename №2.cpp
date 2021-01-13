d9da3e246f0590e0270f69ab603fce7996f5bfb2
ValeriaNigametzianova <71436617+ValeriaNigametzianova@users.noreply.github.com>
2021-01-13 11:30:43 +0300

    Add files via upload

---
+++ №2.cpp
@@ -0,0 +1,38 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int numberDayOfWeek;
+	int day;
+	
+	printf("Enter number days: ");
+	scanf_s("%d", &day);
+
+	numberDayOfWeek = day % 7;
+	printf("Number day of the week: %d. This is ", numberDayOfWeek);
+	switch (numberDayOfWeek)
+	{
+	case 0:
+		printf("sundey");
+		break;
+	case 1:
+		printf("mondey");
+		break;
+	case 2:
+		printf("tuesday");
+		break;
+	case 3:
+		printf("weвnesday");
+		break;
+	case 4:
+		printf("thursday");
+		break;
+	case 5:
+		printf("friday");
+		break;
+	case 6:
+		printf("suterday");
+		break;
+	}
+	return 0;
+}
