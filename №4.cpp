fb5fdca43d51d0e4b45fccda8ac56f3544026980
ValeriaNigametzianova <71436617+ValeriaNigametzianova@users.noreply.github.com>
2021-01-14 21:11:33 +0300

    Add files via upload

---
+++ №4.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int A;
+	int B;
+	int C;
+	int quantitySquares;
+	int freeSpace;
+	printf("Enter A, B, C (in sm): ");
+	scanf_s("%d%d%d", &A, &B, &C);
+	if ((A <= 0) || (B <= 0) || (C <= 0))
+	{
+		printf("A, B, C must be more then 0");
+	}
+	else
+	{
+		quantitySquares = (A / C) * (B / C);
+		freeSpace = A * B - C * C * quantitySquares;
+		printf("The qantity of squares is %d\nThe free space in AxB is %d sm", quantitySquares, freeSpace);
+	}
+	return 0;
+}
