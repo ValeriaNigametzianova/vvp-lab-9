fe1183b8fe88cc80303ff3642c9d5645daf8ca05
ValeriaNigametzianova <71436617+ValeriaNigametzianova@users.noreply.github.com>
2021-01-14 21:48:58 +0300

    Add files via upload

---
+++ №5.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include<locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int year;
+	int century;
+	printf("Введите номер года: ");
+	scanf_s("%d", &year);
+
+	if ((year / 100 + 1) < 0)
+	{
+		century = (year / 100 - 1) * (-1);
+		printf("%d год относится к %d веку до н.э", year, century);
+	}
+	else
+	{
+		century = (year / 100 + 1) ;
+		printf("%d год относится к %d столетию", year, century);
+	}
+	
+	return 0;
+}
